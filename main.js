c92474ac6a4b5a2ae923363f0bd20cc95f1dc138
Misbah Haque <misbahmuneer2003@gmail.com>
2025-06-24 09:54:02 +0500

    Update

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 class Block {
-    constructor(index, timestamp, DataTransfer, poreviousHash = '') {
+    constructor(index, timestamp, DataTransfer, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.DataTransfer = DataTransfer;
+        this.previousHash = previousHash;
 
     }
 }
