60d5a13f2130096908be273b453404b0e5a5ba7b
Misbah Haque <misbahmuneer2003@gmail.com>
2025-06-24 09:58:37 +0500

    Update

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 const SHA256 = require('crypto-js/sha256');
 
 class Block {
-    constructor(index, timestamp, DataTransfer, previousHash = '') {
+    constructor(index, timestamp, DataTransfer, data, previousHash = '') {
         this.index = index;
         this.timestamp = timestamp;
+        this.data = data;
         this.DataTransfer = DataTransfer;
         this.previousHash = previousHash;
         this.Hash = '';
 
     }
     CalculateHash() {
-
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data))
     }
 }
