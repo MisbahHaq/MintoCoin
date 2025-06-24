540c3eefe52121ba3d98b6941a9f349241b9e196
Misbah Haque <misbahmuneer2003@gmail.com>
2025-06-24 11:58:14 +0500

    Generated Public & Private Keys

--- src/blockchain.js
+++ src/blockchain.js
@@ -94,18 +94,5 @@ class Blockchain {
     }
 }
 
-let mintoCoin = new Blockchain();
-
-
-mintoCoin.createTransaction(new Transaction('address1', 'address2', 100)); // address1 would be the public key of someones wallet
-mintoCoin.createTransaction(new Transaction('address2', 'address1', 50));
-
-console.log('\n Starting the Mining...');
-mintoCoin.minePendingTransactions("Misbah's Address");
-console.log('\nBalance of Misbah is', mintoCoin.getBalanceOfAddress("Misbah's Address"));
-
-
-console.log('\n Starting the Mining again...');
-mintoCoin.minePendingTransactions("Misbah's Address");
-
-console.log('\nBalance of Misbah is', mintoCoin.getBalanceOfAddress("Misbah's Address"));
+module.exports.Blockchain = Blockchain;
+module.exports.Transaction = Transaction;
